5e3643794b9c69093d3269274006496746bbb43e
psong1 <paulsong29@gmail.com>
2023-01-08 20:14:09 -0500

    fixed js path in index

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var navlinks = document.getElementById("navlinks")
+
+function showMenu() {
+    document.getElementById("navlinks").style.right = "0";
+}
+
+function hideMenu() {
+    document.getElementById("navlinks").style.right = "-200px";
+}
